cfd3af120c7843b8c354131b10c99682fca812f9
Erica Chambers <53728223+ebc1201@users.noreply.github.com>
2022-08-02 09:43:56 -0500

    adjustments to colors

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 from tkinter import *
+from cell import Cell
 import settings
 import utilities
 
+
 root = Tk()
 # Override window settings
-root.configure(bg="red")
-root.geometry(f"{settings.width} x {settings.height}")
+root.configure(bg="black")
+root.geometry(f"{settings.width}x{settings.height}")
 root.title("Minesweeper")
 root.resizable(False, False)
 
 top_frame = Frame(
-    root, bg="red", width=settings.width, height=utilities.height_prct(25)
+    root, bg="black", width=settings.width, height=utilities.height_prct(25)
 )
 top_frame.place(x=0, y=0)
 
 left_frame = Frame(
-    bg="blue", width=utilities.width_prct(25), height=utilities.height_prct(25)
+    bg="black", width=utilities.width_prct(25), height=utilities.height_prct(25)
 )
 left_frame.place(x=0, y=utilities.height_prct(25))
 
 center_frame = Frame(
-    root, bg="green", width=utilities.width_prct(75), height=utilities.width_prct(75)
+    root, bg="black", width=utilities.width_prct(75), height=utilities.width_prct(75)
 )
 center_frame.place(x=utilities.width_prct(25), y=utilities.height_prct(25))
 
+c1 = Cell()
 
 # Run window
 root.mainloop()
